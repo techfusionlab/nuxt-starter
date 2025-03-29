d12422645e6baab752cb0a8b41e4a3b737d7e38a
Xiaobing Zhu <Hello16369@163.com>
2025-03-29 11:12:30 +0800

    feat(nuxt): integrate PrimeVue components and TailwindCSS
    - Add PrimeVue forms and icons to the project
    - Import TailwindCSS and TailwindCSS PrimeUI
    - Update Nuxt configuration to include PrimeVue module
    - Add script setup for Vue ref
    - Update CSS to import PrimeVue styles

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+const value = ref('')
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Button label="Hello World" />
+    <AutoComplete v-model="value" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
+import Aura from '@primeuix/themes/aura'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils', '@nuxt/eslint', '@nuxt/fonts', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/test-utils',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@pinia/nuxt',
+    '@primevue/nuxt-module',
+  ],
   pinia: {
     storesDirs: ['./store/**'],
   },
@@ -11,4 +18,15 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
   css: ['~/assets/styles/main.css'],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        cssLayer: {
+          name: 'primevue',
+          order: 'theme, base, primevue',
+        },
+      },
+    },
+  },
 })
